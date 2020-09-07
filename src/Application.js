8cab53ca637ca69f80bb0289463f1bbca1d4b394
MD Amdadul Islam <58988171+amdadul3036@users.noreply.github.com>
2020-09-07 04:05:44 -0700

    Wrapped

--- src/Application.js
+++ src/Application.js
@@ -3,7 +3,9 @@ import React from 'react';
 const Application = () => {
     return (
         <div>
-            
+            <h1>Hello JavaScript</h1>
+            <p>Hope you will accept me </p>
+            <h4>HAHAHAHAHA</h4>
         </div>
     );
 };
