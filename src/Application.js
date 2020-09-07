d57f51989ce79e53ef70349ae5ddc5a36dde52c0
MD Amdadul Islam <58988171+amdadul3036@users.noreply.github.com>
2020-09-07 04:13:28 -0700

    Dynamic

--- src/Application.js
+++ src/Application.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
 const Application = () => {
+
+    var language = "Python";
+    language = "JavaScript";
     return (
         <div>
-            <h1>Hello JavaScript</h1>
+            <h1>Hello {language}</h1>
             <p>Hope you will accept me </p>
             <h4>HAHAHAHAHA</h4>
         </div>
