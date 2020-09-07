8cc101640bb5f228c7f2dc03ba2be5a58ae42af4
MD Amdadul Islam <58988171+amdadul3036@users.noreply.github.com>
2020-09-07 06:04:34 -0700

    Trying Usestate

--- src/Application.js
+++ src/Application.js
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Application = () => {
 
-    var language = "Python";
-    language = "JavaScript";
+    var count = 0;
+
+    function handleClick (){
+        const [click , setClick] = useState(0);
+    }
     return (
         <div>
-            <h1>Hello {language}</h1>
-            <p>Hope you will accept me </p>
-            <h4>HAHAHAHAHA</h4>
+            <h1>Number of click is {count}</h1>
+            <button onClick = {handleClick}>Click Me</button>
         </div>
     );
 };
